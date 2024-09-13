c9eddda14c5b491ee186bdacbbc2d3ff2156ac5f
Ancyjemi <132878652+Ancyjemi@users.noreply.github.com>
2024-09-13 10:25:35 +0530

    updated

---
+++ es.js
@@ -0,0 +1,10 @@
+function function1() {
+  console.log("function1");
+}
+const function2 = () => {
+  console.log("function2");
+};
+const function3 = () => console.log("function3");
+function1();
+function2();
+function3();
